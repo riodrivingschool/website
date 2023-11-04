91ee24b7550e2090cf4eda9db917c8f8a97ad8b5
byjohnnyr7 <112647315+byjohnnyr7@users.noreply.github.com>
2023-11-04 01:44:06 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const dropItems = document.getElementById('drop-items')
 
 new Sortable(dropItems, {
-   animation: 250,
+   animation: 8s linear infinite;
    chosenClass: "sortable-chosen",
    dragClass: "sortable-drag",
 });
